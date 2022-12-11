b4494cdc4e2baf946060be82fc46a3e5c61e01f7
unknowntands <119790096+unknowntands@users.noreply.github.com>
2022-12-12 06:36:58 +0800

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -188,26 +188,13 @@
 								    		<div class="col-lg-6">
 								    			<div class="card card-dashboard">
 								    				<div class="card-body">
-								    					<h3 class="card-title">Billing Address</h3><!-- End .card-title -->
-
-														<p>User Name<br>
-														User Company<br>
-														John str<br>
-														New York, NY 10001<br>
-														1-234-987-6543<br>
-														yourmail@mail.com<br>
-														<a href="#">Edit <i class="icon-edit"></i></a></p>
-								    				</div><!-- End .card-body -->
-								    			</div><!-- End .card-dashboard -->
-								    		</div><!-- End .col-lg-6 -->
-
-								    		<div class="col-lg-6">
-								    			<div class="card card-dashboard">
-								    				<div class="card-body">
 								    					<h3 class="card-title">Shipping Address</h3><!-- End .card-title -->
 
-														<p>You have not set up this type of address yet.<br>
-														<a href="#">Edit <i class="icon-edit"></i></a></p>
+														<p><?php echo $_SESSION["name"]; ?><br>
+														<?php echo $_SESSION["number"]; ?><br>
+														<?php echo $_SESSION["address1"]; ?><br>
+														<?php echo $_SESSION["address2"]; ?><br>
+														<a href="update-info.php">Edit <i class="icon-edit"></i></a></p>
 								    				</div><!-- End .card-body -->
 								    			</div><!-- End .card-dashboard -->
 								    		</div><!-- End .col-lg-6 -->
